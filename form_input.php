726eee33ddcc5453b7815d9995b087f83763d7fb
AHMAD ROSYIDIN <81254764+ahmadrosyidin@users.noreply.github.com>
2021-05-05 20:49:59 +0700

    Update form_input.php

--- form_input.php
+++ form_input.php
@@ -14,14 +14,5 @@
 <?php
 echo 'Selamat Datang ' . $_POST['nama'];
 ?>
-
-<?php
-$gaji = 1000000;
-$pajak = 0.1;
-$thp = $gaji - ($gaji*$pajak);
-echo "Gaji sebelum pajak = Rp. $gaji <br>";
-echo "Gaji yang dibawa pulang = Rp. $thp";
-?>
-
 </body>
 </html>
